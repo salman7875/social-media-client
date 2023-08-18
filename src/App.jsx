5e1e8ff75f59610c225645128df2ab775eb6378b
salmanansari.altpi <salmanansari.altpi@gmail.com>
2023-08-18 15:30:50 +0530

    profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import Register from './component/Register/Register'
-// import Login from './component/login/Login      '
+// import Login from './component/login/Login'
 import Feed from './component/Feed/Feed'
+import Profile from './component/Profile/Profile'
 import Navbar from './layout/Navbar/Navbar'
 
 const App = () => {
   return (
     <>
-      <Feed />
+      <Profile />
       <Navbar />
     </>
   )
---
+++ src/component/Profile/Profile.jsx
@@ -0,0 +1,66 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import "./profile.scss";
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <header>
+        <div className="left">
+          <p>Salman__76__</p>
+        </div>
+        <div className="right">
+          <button>Logout</button>
+          <MenuIcon className="icon" />
+        </div>
+      </header>
+      <div className="inner-profile">
+        <div className="left">
+            <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        </div>
+        <div className="right">
+            <ul>
+                <li>
+                    <a href="#">10</a>
+                    <p>Posts</p>
+                </li>
+                <li>
+                    <a href="#">10</a>
+                    <p>Followers</p>
+                </li>
+                <li>
+                    <a href="#">10</a>
+                    <p>Followings</p>
+                </li>
+            </ul>
+        </div>
+      </div>
+      <div className="user-info">
+        <p className="username">Salman__76__</p>
+        <p className="bio">Kisi ka bhai kisi ki jaan 🔥🔥🔥</p>
+      </div>
+      <div className="actions">
+        <button>Edit Profile</button>
+        <button>Share Profile</button>
+      </div>
+      <hr />
+
+      <div className="posts">
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+        <img src="https://avatars.githubusercontent.com/u/78205495?s=48&v=4" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
